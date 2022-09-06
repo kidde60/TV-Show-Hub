fa645f26a29c0ae58f0de8bb04ea81b0e8fc6ccd
DJ-MrJay <97578860+DJ-MrJay@users.noreply.github.com>
2022-09-06 21:19:24 +0300

    Add CSS styling

--- src/index.js
+++ src/index.js
@@ -1,40 +1,44 @@
-// import './style.css';
-const path = 'https://imdb8.p.rapidapi.com/auto-complete?q=game';
+import './style.css';
+const path = 'https://imdb8.p.rapidapi.com/auto-complete?q=cruise';
 const container = document.querySelector('.container')
 const list = document.createElement('ul')
 
-
 const getMovies = async () => {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '4e9eb6ac39mshafc132e8850bb83p183f0bjsn09e883c16e80',
-            'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-        }
-    };
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '4e9eb6ac39mshafc132e8850bb83p183f0bjsn09e883c16e80',
+      'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+    }
+  };
 
-    fetch(path, options)
-        .then(response => response.json())
-        .then(data => {
-            const allMovies = data.d;
-            allMovies.forEach((card) => {
-                const listItem = document.createElement('li')
-                listItem.innerHTML = '';
-                listItem.innerHTML = `
-                            <div>
-                            <img src="${card.i.imageUrl}" alt="${card.l}">
-                            <h1 class="title">${card.l}</h1>
-                            <p class="likes">
-                            <i class="fa-solid fa-heart"></i>
-                            Likes
-                            </p>
-                            </div>
-                            <button class="btn">Comments</button>
-                        `
-                list.appendChild(listItem);
-                container.appendChild(list)
-            });
-        })
+  fetch(path, options)
+    .then(response => response.json())
+    .then(data => {
+      const allMovies = data.d;
+      allMovies.forEach((card) => {
+        const listItem = document.createElement('li')
+        listItem.innerHTML = '';
+        listItem.innerHTML = `
+          <div>
+            <img src="${card.i.imageUrl}" alt="${card.l}">
+            <div class="card-text">
+              <div class="title-like">
+                <p class="title">
+                  ${card.l}<br>${card.y}
+                </p>
+                <p class="likes">
+                  Like
+                  <i class="far fa-heart"></i>
+                </p>
+              </div>
+              <hr>
+              <button class="btn">Comments</button>
+            </div>`
+        list.appendChild(listItem);
+        container.appendChild(list)
+      });
+    })
 }
 getMovies();
 
