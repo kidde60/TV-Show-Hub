e07833d99e6fe07e468c077830b2ae37e3cf3b3d
kidde60 <kgeorgewilliam60@gmail.com>
2022-09-09 12:45:38 +0300

    Add like button functionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 // import './style.css';
+
+
+
 // import Logo from './assets/logo.svg';
 
-// const path = 'https://imdb8.p.rapidapi.com/auto-complete?q=friends';
+// const uniqueID = '41z7rNWXbEPeMpQVJIbo'
+
+
 const container = document.querySelector('.container');
 const navbar = document.querySelector('.navbar a');
 const list = document.createElement('ul');
-
+const popup = document.querySelector('.modal-container')
 // const pageLogo = document.createElement('img');
 // pageLogo.setAttribute('class', 'logo');
-// pageLogo.setAttribute('src', Logo);
+// pageLogo.setAttribute('src', Logo);)
 // navbar.appendChild(pageLogo);
 const url = 'https://api.tvmaze.com/shows';
 const getData = async () => {
@@ -17,10 +22,18 @@ const getData = async () => {
 
   return data.slice(3, 15);
 }
+window.onload = getData()
+const movieCount = (arr) => arr.length;
 
 const display = async () => {
-  const data = await getData()
+  const data = await getData();
+  const likes = await fetchLikeApi();
+  const movieNumber = document.querySelector('.movie-number');
+  movieNumber.innerHTML = `(${movieCount(data)})`;
   data.forEach((card) => {
+    const cardLikes = likes.find((like) => like.item_id === card.id)
+    const { id } = card;
+    let liveCount = +cardLikes?.likes;
     // console.log(card)
     const listItem = document.createElement('li');
     listItem.innerHTML = '';
@@ -33,22 +46,51 @@ const display = async () => {
                   ${card.name}
                 </p>
                 <p class="likes">
-                  Like
+                  
                   <i class="far fa-heart"></i>
+                  <p class="likes">${liveCount} Likes</p>
                 </p>
               </div>
               <hr>
-              <button class="openBtn">Comments</button>
+              <button class="openBtn" id=${id}>Comments</button>
             </div>`;
+
+    const likeBtn = listItem.querySelectorAll('.fa-heart');
+    likeBtn.forEach((button) => {
+      button.addEventListener('click', (btn) => {
+        const liveCountElement = listItem.getElementsByClassName('likes');
+        liveCount += 1;
+        liveCountElement.innerHTML = `${liveCount} likes`;
+        submitNewLike(id);
+        btn.disabled = true;
+        button.style.color = 'red';
+        location.reload()
+      },
+      )
+    })
     list.appendChild(listItem);
     container.appendChild(list);
-    // const commentBtn = document.querySelectorAll('.openBtn')
-    // commentBtn.forEach((btn) => {
-    //   btn.addEventListener('click',)
-    // })
   })
 
 }
 display()
 
+const uniqueID = 'MjgCDPvMKfBMbwFq4McF'
+const fetchLikeApi = async () => {
+  const getLikeResult = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${uniqueID}/likes`)
+    .then((res) => res.json());
+  return getLikeResult;
+};
+
+const submitNewLike = async (id) => {
+  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${uniqueID}/likes`, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: id,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+};
 
