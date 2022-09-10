1136de4dea7378dbdbf3411d83f40e15060b0fc0
DJ-MrJay <97578860+DJ-MrJay@users.noreply.github.com>
2022-09-10 23:25:44 +0300

    Delete unused code

--- src/index.js
+++ src/index.js
@@ -91,17 +91,6 @@ const submitNewLike = async (id) => {
 };
 // popup
 
-// const postApp = async () => {
-//   const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/', {
-//     method: 'POST',
-//     headers: {
-//       'Content-type': 'application/json; charest=UTF-8',
-//     },
-//   });
-//   const data = await response.json();
-//   return data;
-// };
-
 const postComments = async (id, name, com) => {
   await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4va6c4ouZmpzSETsANV3/comments', {
     method: 'POST',
