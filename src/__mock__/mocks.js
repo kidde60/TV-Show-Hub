0eda9ad274af0a198e6bf33baee395beb4eb7b3c
kidde60 <kgeorgewilliam60@gmail.com>
2022-09-10 22:54:06 +0300

    Add mocks

---
+++ src/__mock__/mocks.js
@@ -0,0 +1,36 @@
+const movieApi = () => Promise.resolve([
+    {
+        title: 'Under the Doom',
+        weight: 60,
+        summary: 'lorem ipsum dolor sit',
+    },
+    {
+        title: 'Inside the Doom',
+        weight: 50,
+        summary: 'lorem ipsum dolor sit',
+    },
+    {
+        title: 'swallowed by the Doom',
+        weight: 100,
+        summary: 'lorem ipsum dolor sit',
+    },
+]);
+
+const getComments = () => Promise.resolve([
+    {
+        id: 1,
+        name: 'William',
+        comment: 'lorem ipsum dolor sit',
+    },
+    {
+        id: 2,
+        name: 'Jonah',
+        comment: 'lorem ipsum dolor sit',
+    },
+    {
+        id: 3,
+        name: 'Kidde',
+        comment: 'lorem ipsum dolor sit',
+    },
+]);
+export { movieApi, getComments };
