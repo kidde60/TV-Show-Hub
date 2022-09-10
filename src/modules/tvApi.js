c9ebba3b749b7be9e1260dc15f72e5efc2f9aa47
DJ-MrJay <97578860+DJ-MrJay@users.noreply.github.com>
2022-09-10 22:53:44 +0300

    Add tests

--- src/modules/tvApi.js
+++ src/modules/tvApi.js
@@ -1,5 +1,5 @@
 export const getData = async (id) => {
-    const movieInfo = await fetch(`https://api.tvmaze.com/shows/${id}`).then((result) => result.json());
-    return movieInfo;
-    };
+	const movieInfo = await fetch(`https://api.tvmaze.com/shows/${id}`).then((result) => result.json());
+	return movieInfo;
+};
 
