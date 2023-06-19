657c98f710ad62af34c9b4fd9dd13392a35f2967
Jonah Wambua <djmrjay@gmail.com>
2023-06-20 01:49:07 +0300

    Improve visual

--- src/index.js
+++ src/index.js
@@ -1,80 +1,100 @@
-/* eslint-disable no-use-before-define */
-
 import './style.css';
 
 document.addEventListener('DOMContentLoaded', () => {
-  displayMovie();
+  displayCards();
 });
 
-//   Home page
-const movieApi = async () => {
+const tvShowApi = async () => {
   const fetchResult = await fetch('https://api.tvmaze.com/shows');
-  const ShowResult = await fetchResult.json();
-  return ShowResult.slice(2, 15);
+  const showResult = await fetchResult.json();
+  return showResult.slice(8, 62);
 };
 
-window.onload = movieApi();
+window.onload = tvShowApi();
 
-const contCount = (array) => array.length;
+const countShows = (array) => array.length;
 
-const displayMovie = async () => {
-  const Movies = await movieApi();
+const displayCards = async () => {
+  const shows = await tvShowApi();
   const fetchLikes = await fetchLikeApi();
 
-  const movieLength = document.querySelector('.movie-number');
-  movieLength.innerText = `Movies(${contCount(Movies)})`;
+  const showCount = document.querySelector('.show-count');
+  showCount.innerText = `Displaying ${countShows(shows)} Shows`;
 
-  Movies.forEach((card) => {
+  shows.forEach((card) => {
     const cardLikes = fetchLikes.find((like) => like.item_id === card.id);
     const { id } = card;
 
-    let liveCount = +cardLikes?.likes;
+    let liveCount = cardLikes?.likes ?? 0;
 
-    const Container = document.querySelector('.modal-container');
-    const listItem = document.createElement('div');
+    const container = document.querySelector('.container');
+    const listCards = document.createElement('div');
+    listCards.classList.add('card-item');
 
-    listItem.innerHTML = '';
-    listItem.innerHTML = `<div>
-                          <img src=${card.image.original} alt=${card.name}>
-                        </div>
-                        <a href="${card.officialSite}" class="movie-title">${card.name}</a>
-                        <div class="movie-info">
-                          <p>${card.weight}mb</p>
-                          <i class="fa fa-heart"  aria-hidden="true"></i>
-                          <p class = "totalLikes">${liveCount} likes</p>
-                        </div>
-                        <button id = ${id} class = "commentBtn">Comments</button>`;
+    listCards.innerHTML = `
+      <div>
+        <img src=${card.image.original} alt=${card.name}>
+      </div>
+      <div class="card-text">
+        <a href="${card.officialSite}" class="show-title" target="_blank">${card.name}</a>
+        
+        <div class="rating-container">
+          <span class="rating-label">Rating </span>
+          <span class="rating-value">${card.rating.average}</span>
+        </div>
 
-    const likeBtn = listItem.querySelectorAll('.fa-heart');
+        <div class="show-info">
+          <div class="likes">
+            <i class="fa fa-heart" aria-hidden="true"></i>
+            <p class="totalLikes">${liveCount} likes</p>
+          </div>
+          <div class="comment">
+            <i class="fa fa-comment" aria-hidden="true"></i>
+            <button id="${id}" class="comment-btn">Comment</button>
+          </div>
+        </div>
+      </div>
+    `;
+
+    const likeBtn = listCards.querySelectorAll('.fa-heart');
     likeBtn.forEach((button) => {
-      button.addEventListener('click', (btn) => {
-        const liveCountElement = listItem.getElementsByClassName('totalLikes')[0];
+      button.addEventListener('mouseenter', () => {
+        button.style.cursor = 'pointer';
+      });
+
+      button.addEventListener('click', () => {
+        const liveCountElement = listCards.querySelector('.totalLikes');
         liveCount += 1;
-        liveCountElement.innerHTML = `${liveCount} likes`;
+        liveCountElement.textContent = getLikesText(liveCount);
         submitNewLike(id);
-        btn.disabled = true;
+        button.disabled = true;
         button.style.color = 'red';
       });
     });
 
-    // cardUL.appendChild(cardLI);
-    Container.appendChild(listItem);
+    container.appendChild(listCards);
 
     const totalLikes = document.querySelectorAll('.totalLikes');
     totalLikes.forEach((like) => {
-      if (like.innerText === 'undefined likes') {
-        like.innerText = '0 like';
-      } else if (like.innerText === '1 likes') {
-        like.innerText = '1 like';
-      }
+      const count = parseInt(like.innerText);
+      like.innerText = getLikesText(count);
     });
+
+    function getLikesText(count) {
+      if (count === 1) {
+        return `${count} like`;
+      } else {
+        return `${count} likes`;
+      }
+    }
   });
 };
-// likes
+
 const tvId = 'MjgCDPvMKfBMbwFq4McF';
 
 const fetchLikeApi = async () => {
-  const getLikeResult = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${tvId}/likes`).then((res) => res.json());
+  const getLikeResult = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${tvId}/likes`)
+    .then((res) => res.json());
   return getLikeResult;
 };
 
@@ -89,15 +109,14 @@ const submitNewLike = async (id) => {
     },
   });
 };
-// popup
 
-const postComments = async (id, name, com) => {
+const postComments = async (id, name, comment) => {
   await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4va6c4ouZmpzSETsANV3/comments', {
     method: 'POST',
     body: JSON.stringify({
       item_id: id,
       username: name,
-      comment: com,
+      comment: comment,
     }),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
@@ -106,91 +125,102 @@ const postComments = async (id, name, com) => {
 };
 
 const getComments = async (id) => {
-  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4va6c4ouZmpzSETsANV3/comments?item_id=${id}`).then((res) => res.json());
+  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4va6c4ouZmpzSETsANV3/comments?item_id=${id}`)
+    .then((res) => res.json());
   return response;
 };
 
 document.addEventListener('click', async (e) => {
-  if (!e.target.matches('.commentBtn')) {
-    return;
-  }
-  const { id } = e.target;
-  const comList = await getComments(id);
-  const MoveInfo = await getData(id);
-  await display(MoveInfo, id, comList);
-  const container = document.querySelector('.modal-container');
-  container.style.display = 'none';
+  if (e.target.matches('.comment-btn')) {
+    const id = e.target.id;
+    const commentList = await getComments(id); // Wait for getComments to complete and return the comment list
+    const showInfo = await getData(id);
+    await displayPopup(showInfo, id, commentList);
+    const container = document.querySelector('.container');
+    container.style.display = 'none';
+    const popup = document.querySelector('.popup');
+    popup.style.display = 'block';
 
-  displayCom(comList);
+    displayComments(commentList);
+  }
 });
 
-// class Popup {
 const getData = async (id) => {
-  const movieInfo = await fetch(`https://api.tvmaze.com/shows/${id}`).then((result) => result.json());
-  return movieInfo;
+  const showInfo = await fetch(`https://api.tvmaze.com/shows/${id}`).then((result) => result.json());
+  return showInfo;
 };
 
-const countComments = (number) => number.length;
+const countComments = (comments) => comments.length;
 
-const display = async (movieInfo, id, comList) => {
-  const movie = await movieInfo;
-  const array = await comList;
+const displayPopup = async (showInfo, id, commentList) => {
+  const show = await showInfo;
+  const comments = await commentList;
   const popup = document.querySelector('.popup');
   popup.innerHTML = `
-      <div class="description">
-      <button type="button" class="closeBtn">X</button>
-        <div>
-          <a href="${movie.network.officialSite}"><img src="${movie.image.medium}" class = "image" id = "movie-img" alt=""></a>
-          <div class = "summary-tag">
-          <h1>${movie.name}</h1>
-          ${movie.summary}
-        </div>
-        <div>
-        <h3 class = "class-heading"> All Comments (${countComments(array)})</h3>
-        <ul class ="display-comments">
-        </ul>
-      </div>
-            <div class ="form" >
-            <h2> Add a comment </h2>
-              <label for="fname">Name:</label><br>
-              <input type="text" id="fname" name="fname" placeholder = "Enter your name"><br><br>
-              <label for="comment">Comment:</label><br>
-              <textarea name="comment" id="comment" cols="20" rows="3" placeholder = "Enter comment"></textarea>
-              <button id = ${id} class= "submit" type="submit"> Submit</button>
+    <div class="modal-container">
+      <div class="modal">
+        <span class="close-btn"><i class="fas fa-times"></i></span>
+        <div class="modal-content">
+          <div class="modal-image">
+            <a href="#" id="official-site-link"><img src="${show.image.original}" id="show-img" alt=""></a>
+          </div>
+          <div class="modal-details">
+            <div class="summary-tag">
+              <h1>${show.name}</h1>
+              <p>${show.summary}</p>
+              <br>
             </div>
-          
-        </div>
-        
-      </div>
-      
-        
-      </ul>`;
+            
+            <div>
+              <h3>${countComments(comments) === 0 ? 'No Comments' : `${countComments(comments)} Comment${countComments(comments) === 1 ? '' : 's'}`}</h3>
+              <ul class="display-comments"></ul>
+              <br>
+            </div>
 
-  const close = document.querySelector('.closeBtn');
-  close.addEventListener('click', () => {
-    document.querySelector('.modal-container').style.display = 'grid';
-    document.querySelector('.popup').style.display = 'none';
+            <div>
+              <h2>Add a comment</h2>
+              <br>
+              <div class="form">
+                <input type="text" id="fname" name="fname" placeholder="Enter your name"><br><br>
+                <textarea name="comment" id="comment" cols="20" rows="3" placeholder="Enter comment"></textarea>
+                <button id="${id}" class="submit" type="submit">Submit</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  const closeBtn = document.querySelector('.close-btn');
+  closeBtn.addEventListener('click', () => {
+    const container = document.querySelector('.container');
+    container.style.display = 'grid';
+    popup.style.display = 'none';
   });
 };
 
-const displayCom = async (MoveInfo) => {
-  const commentList = document.querySelector('.display-comments');
-  commentList.innerHTML = '';
-  const array = await MoveInfo;
-  array.forEach((item) => {
-    commentList.innerHTML += `<li class = "listCom">${item.username}: ${item.comment} <br> :${item.creation_date.slice(-5, -1)}</li>`;
+const displayComments = async (commentList) => {
+  const commentsContainer = document.querySelector('.display-comments');
+  commentsContainer.innerHTML = '';
+  const comments = await commentList;
+  comments.forEach((comment) => {
+    commentsContainer.innerHTML += `
+      <div class="text-bubble">
+        <li>${comment.username}: ${comment.comment}<br> ${comment.creation_date}</li>
+      </div>
+    `;
   });
 };
 
 document.addEventListener('click', async (e) => {
-  if (!e.target.matches('.submit')) {
-    return;
+  if (e.target.matches('.submit')) {
+    e.preventDefault();
+    const name = document.getElementById('fname').value;
+    const comment = document.getElementById('comment').value;
+    const id = e.target.id;
+    await postComments(id, name, comment);
+    const commentList = await getComments(id);
+    displayComments(commentList);
   }
-  e.preventDefault();
-  const name = document.getElementById('fname').value;
-  const com = document.getElementById('comment').value;
-  const { id } = e.target;
-  await postComments(id, name, com);
-  const comList = await getComments(id);
-  displayCom(comList);
 });
